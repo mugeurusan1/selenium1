d8b17b05cb4fae1cd1e7e4c132e29822aebdd6b7
muge <mugeurusan@yahoo.com.tr>
2022-07-09 13:58:00 +0300

    for d

--- src/Gun01/_01isim.java
+++ src/Gun01/_01isim.java
@@ -4,5 +4,8 @@ public class _01isim {
     public static void main(String[] args) {
         System.out.println("merhaba dunya");
 
+        for(int i=1;i<=10;i++)
+        System.out.println(i);
+
     }
 }
